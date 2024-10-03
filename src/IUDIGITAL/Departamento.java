0bfa566acf4598469c5b2e10cf3d865c97555a42
dahar <harold.velandia@est.iudigital.edu.co>
2024-10-02 19:26:27 -0500

    subiendo el proyecto al espacio de trabajo en github

---
+++ src/IUDIGITAL/Departamento.java
@@ -0,0 +1,35 @@
+package IUDIGITAL;
+
+import java.util.ArrayList;
+
+public class Departamento {
+    private int idDepartamento;
+    private String nombre;
+    private ArrayList<Empleado> listaEmpleados;
+
+    public Departamento(int idDepartamento, String nombre) {
+        this.idDepartamento = idDepartamento;
+        this.nombre = nombre;
+        this.listaEmpleados = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void asignarEmpleado(Empleado empleado) {
+        listaEmpleados.add(empleado);
+        empleado.asignarDepartamento(this);
+    }
+
+    public void eliminarEmpleado(Empleado empleado) {
+        listaEmpleados.remove(empleado);
+    }
+
+    public void visualizarEmpleados() {
+        System.out.println("Empleados en el departamento " + nombre + ":");
+        for (Empleado empleado : listaEmpleados) {
+            System.out.println(empleado);
+        }
+    }
+}
---
+++ src/IUDIGITAL/Empleado.java
@@ -0,0 +1,59 @@
+package IUDIGITAL;
+
+public abstract class Empleado {
+    protected int idEmpleado;
+    protected String nombre;
+    protected String apellido;  // Nuevo atributo
+    protected String tipoEmpleado;
+    protected Departamento departamento;
+
+    public Empleado(int idEmpleado, String nombre, String apellido, String tipoEmpleado) {
+        this.idEmpleado = idEmpleado;
+        this.nombre = nombre;
+        this.apellido = apellido;  // Asignación del apellido
+        this.tipoEmpleado = tipoEmpleado;
+    }
+
+    public void asignarDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public void cambiarDepartamento(Departamento nuevoDepartamento) {
+        this.departamento = nuevoDepartamento;
+    }
+
+    public abstract void generarReporteDesempeño();
+
+    @Override
+    public String toString() {
+        return "ID: " + idEmpleado + ", Nombre: " + nombre + " " + apellido + ", Tipo: " + tipoEmpleado + ", Departamento: " + (departamento != null ? departamento.getNombre() : "Sin departamento");
+    }
+}
+
+class EmpleadoPermanente extends Empleado {
+    private double salario;
+
+    public EmpleadoPermanente(int idEmpleado, String nombre, String apellido, double salario) {
+        super(idEmpleado, nombre, apellido, "Permanente");
+        this.salario = salario;
+    }
+
+    @Override
+    public void generarReporteDesempeño() {
+        System.out.println("Generar reporte de desempeño para empleado permanente.");
+    }
+}
+
+class EmpleadoTemporal extends Empleado {
+    private double tarifaPorHora;
+
+    public EmpleadoTemporal(int idEmpleado, String nombre, String apellido, double tarifaPorHora) {
+        super(idEmpleado, nombre, apellido, "Temporal");
+        this.tarifaPorHora = tarifaPorHora;
+    }
+
+    @Override
+    public void generarReporteDesempeño() {
+        System.out.println("Generar reporte de desempeño para empleado temporal.");
+    }
+}
